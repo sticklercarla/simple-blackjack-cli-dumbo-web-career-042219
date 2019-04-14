67e82aee096ed4821ff0c1fa0736eb68c0a8d4be
Carla Stickler <carlastickler@gmail.com>
2019-04-14 20:46:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,8 +63,8 @@ def runner
   welcome
   card_total = initial_round
   while card_total <21
-    hit?(card_total)
-    display_card_total(card_total)
+    card_total = hit?(card_total)
+    card_total = display_card_total(card_total)
   end
   end_game(card_total)
 
